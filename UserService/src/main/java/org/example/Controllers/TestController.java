87cc94918217ebe4b0a52c247084df29a59fdb5b
Dhammika-Mahendra <wmdhammikamahendra@gmail.com>
2024-11-30 19:53:39 +0530

    Method security role based structure

--- UserService/src/main/java/org/example/Controllers/TestController.java
+++ UserService/src/main/java/org/example/Controllers/TestController.java
@@ -1,21 +1,34 @@
 package org.example.Controllers;
 
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/user/test")
+@EnableMethodSecurity
 public class TestController {
-    @GetMapping("seller")
+    @GetMapping("/seller")
     public String testSeller() {
-        return "Test passed";
+        return "Test Seller passed";
     }
 
-    @GetMapping("buyer")
-    @PreAuthorize("hasRole('BUYER')")
+    @GetMapping("/seller/auth")
+    @PreAuthorize("hasRole('SELLER')")
+    public String testSellerAuth() {
+        return "Test Seller auth passed";
+    }
+
+    @GetMapping("/buyer")
     public String testBuyer() {
-        return "Test passed";
+        return "Test Buyer passed";
+    }
+
+    @GetMapping("/buyer/auth")
+    @PreAuthorize("hasRole('BUYER')")
+    public String testBuyerAuth() {
+        return "Test Buyer auth passed";
     }
 }
